390ee8afaecfa491a6f76b0c689230d17184c3bf
kang_eunseong <Eunseong0103@gmail.com>
2023-06-09 13:34:38 +0900

    contentList_Glide(RecyclerView item Click)구현

--- app/src/main/java/com/example/mysolelife/contentsList/ContentModel.kt
+++ app/src/main/java/com/example/mysolelife/contentsList/ContentModel.kt
@@ -3,5 +3,6 @@ package com.example.mysolelife.contentsList
 data class ContentModel(
 
     var title: String = "",
-    var imageUrl : String = ""
+    var imageUrl : String = "",
+    var webUrl : String =""
 )
