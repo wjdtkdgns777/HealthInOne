bda5d86c39b6a988852bdbdc8963cf5e58396d9c
kang_eunseong <Eunseong0103@gmail.com>
2023-06-17 18:07:19 +0900

    06.17 게시글만들기

---
+++ app/src/main/java/com/example/mysolelife/utils/FBref.kt
@@ -0,0 +1,22 @@
+package com.example.mysolelife.utils
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
+
+class FBref {
+
+    companion object{
+
+        private val database = Firebase.database
+
+        val category1 = database.getReference("contents")
+        val category2 = database.getReference("contents2")
+
+        val bookmarkRef = database.getReference("bookmark_list")
+
+        val boardRef = database.getReference("board")
+
+    }
+}
