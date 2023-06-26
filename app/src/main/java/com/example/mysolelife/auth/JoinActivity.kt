9099e724e56aa534666758c56372db7bc3adfa83
DESKTOP-E152GOJ\aaa <wjdtkdgns234@naver.com>
2023-06-26 20:00:33 +0900

    Sign in update

--- app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
@@ -1,14 +1,17 @@
 package com.example.mysolelife.auth
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.mysolelife.MainActivity
 import com.example.mysolelife.R
 import com.example.mysolelife.databinding.ActivityJoinBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -27,13 +30,18 @@ class JoinActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
 
         binding.joinBtn.setOnClickListener {
+            val username = binding.usernameArea.text.toString()
             val email = binding.emailArea.text.toString()
             val pwd1 = binding.pwdArea1.text.toString()
             val pwd2 = binding.pwdArea2.text.toString()
             var isGoToJoin = true
 
             // 값이 비어 있는지 확인
-            if(email.isEmpty()) {
+            if(username.isEmpty()) {
+                Toast.makeText(this, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+            else if(email.isEmpty()) {
                 Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show()
                 isGoToJoin = false
             }
@@ -57,20 +65,30 @@ class JoinActivity : AppCompatActivity() {
             }
 
             if(isGoToJoin){
-                auth.createUserWithEmailAndPassword(email, pwd1).addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, display a message to the user.
-                        Toast.makeText(this, "auth succeed", Toast.LENGTH_SHORT).show()
+                auth.createUserWithEmailAndPassword(email, pwd1)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            val user = auth.currentUser
+                            val profileUpdates = UserProfileChangeRequest.Builder()
+                                .setDisplayName(username)
+                                .build()
 
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                        startActivity(intent)
+                            user?.updateProfile(profileUpdates)
+                                ?.addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Log.d(TAG, "User profile updated.")
+                                        // Navigate to MainActivity
+                                        val intent = Intent(this, MainActivity::class.java)
+                                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                        startActivity(intent)
+                                    }
+                                }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                    else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(this, "auth failed", Toast.LENGTH_SHORT).show()
-                    }
-                }
             }
         }
     }
